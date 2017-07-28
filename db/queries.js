872a44ab41443170ea029d274fec1b5dce0ec97a
Jason Clifton <j.clifton1473@gmail.com>
2017-07-28 14:56:37 -0600

    added route fixture

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const knex = require('./knex');
 
 module.exports = {
 	getAllSandcastles() {
-		return knex('sandcastles').join('users', 'user_id', '=', 'users.id')
+		return knex('sandcastles').select('sandcastles.id as id', 'sandcastles.url as url', 'users.id as user_id', 'sandcastles.description as description', 'sandcastles.name as name').join('users', 'user_id', '=', 'users.id')
 	},
 	addNewSandcastle(sandcastle) {
 		return knex('sandcastles').insert(sandcastle, '*');
